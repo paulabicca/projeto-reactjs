1e80ea2af05bb07f628f4c27dd3f4bad4e13216e
Paula <paula.bicca@gmail.com>
2019-05-19 23:02:40 -0300

    Buscando produtos

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+const Header = () =>(
+    <header id="main-header">JsHunt</header>
+);
+export default Header;
+
+/*
+É a mesma coisa do de cima
+class Header extends Component {
+    render (){
+        return <h1></h1>
+    }
+}*/
---
+++ src/pages/main/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+export default class Main extends Component {
+    //componentDidMount >> excutado assim que o componente for executado
+    componentDidMount(){
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get('/products');
+
+        console.log(response.data.docs);
+    };
+
+    render (){
+        return <h1>Hello World</h1>
+    }
+}
+
+
+
+
