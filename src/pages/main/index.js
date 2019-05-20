c152aaf39ad914e716e7a43fd7aef209cbb918af
Paula <paula.bicca@gmail.com>
2019-05-20 20:24:29 -0300

    Armazenando e listando produtos

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 
 export default class Main extends Component {
+    /*sempre que houver uma variavel no state, o render sempre vai ficar "ouvindo" a alteração da
+    variavel
+    */
+        state = {
+            products: []
+        }
+
     //componentDidMount >> excutado assim que o componente for executado
     componentDidMount(){
         this.loadProducts();
@@ -10,11 +17,18 @@ export default class Main extends Component {
     loadProducts = async () => {
         const response = await api.get('/products');
 
-        console.log(response.data.docs);
+        //console.log(response.data.docs);
+        this.setState({products: response.data.docs});
     };
-
+    //map para percorrer, map precisa de uma chave única >> key={product._id}
     render (){
-        return <h1>Hello World</h1>
+        return (
+            <div className="product-list">
+            {this.state.products.map(product => (
+                <h2 key={product._id}>{product.title}</h2>
+            ))}
+            </div>
+        )
     }
 }
 
