b6ace70e750d63ae0523b95133c34f0f18802805
Paula Bicca <paula.bicca@gmail.com>
2019-05-31 14:06:07 -0300

    Pag anterior/próxima

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -6,7 +6,9 @@ export default class Main extends Component {
 	variavel
 	*/
 	state = {
-		products: []
+		products: [],
+		productInfo: {},
+		page: 1,
 	}
 
 	//componentDidMount >> excutado assim que o componente for executado
@@ -14,15 +16,38 @@ export default class Main extends Component {
 		this.loadProducts();
 	}
 
-	loadProducts = async () => {
-		const response = await api.get('/products');
-
+	loadProducts = async ( page = 1) => { //definindo o valor padrão da page
+		const response = await api.get(`/products?page=${page}`);
 		//console.log(response.data.docs);
-		this.setState({products: response.data.docs});
+
+		const {docs, ...productInfo } = response.data;
+		this.setState({products: docs, productInfo, page}); //atualiza os valores
 	};
+	prevPage = () => {
+		const { page, productInfo } = this.state;
+
+		if(page === 1) return;
+
+		const pageNumber = page - 1;
+
+		this.loadProducts(pageNumber);
+
+	};
+
+	nextPage = () => {
+		//pegando valores
+		const { page, productInfo } = this.state;
+
+		if(page == productInfo.pages) return;
+
+		const pageNumber = page + 1; //pega a próxima pag
+
+		this.loadProducts(pageNumber);
+	};
+
 	//map para percorrer, map precisa de uma chave única >> key={product._id}
 	render (){
-		const { products } = this.state;
+		const { products, page, productInfo } = this.state;
 
 		return (
 		<div className="product-list">
@@ -33,6 +58,11 @@ export default class Main extends Component {
 					<a href="">Acessar</a>
 				</article>
 			))}
+			<div className="actions">
+				<button disabled={page == 1} onClick={this.prevPage}>Anterior</button>
+				<button disabled={page == productInfo.pages} onClick={this.nextPage}>Próximo</button>
+			</div>
+
 		</div>
 		)
 	}
