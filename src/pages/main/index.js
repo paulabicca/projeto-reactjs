f73e51ebdcd316c86f3e7b27a95c3e7b10a7d0a3
Paula Bicca <paula.bicca@gmail.com>
2019-05-31 12:03:39 -0300

    Listando e estilizando pags

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
-
+import './styles.css';
 export default class Main extends Component {
-    /*sempre que houver uma variavel no state, o render sempre vai ficar "ouvindo" a alteração da
-    variavel
-    */
-        state = {
-            products: []
-        }
+	/*sempre que houver uma variavel no state, o render sempre vai ficar "ouvindo" a alteração da
+	variavel
+	*/
+	state = {
+		products: []
+	}
 
-    //componentDidMount >> excutado assim que o componente for executado
-    componentDidMount(){
-        this.loadProducts();
-    }
+	//componentDidMount >> excutado assim que o componente for executado
+	componentDidMount(){
+		this.loadProducts();
+	}
 
-    loadProducts = async () => {
-        const response = await api.get('/products');
+	loadProducts = async () => {
+		const response = await api.get('/products');
 
-        //console.log(response.data.docs);
-        this.setState({products: response.data.docs});
-    };
-    //map para percorrer, map precisa de uma chave única >> key={product._id}
-    render (){
-        return (
-            <div className="product-list">
-            {this.state.products.map(product => (
-                <h2 key={product._id}>{product.title}</h2>
-            ))}
-            </div>
-        )
-    }
+		//console.log(response.data.docs);
+		this.setState({products: response.data.docs});
+	};
+	//map para percorrer, map precisa de uma chave única >> key={product._id}
+	render (){
+		const { products } = this.state;
+
+		return (
+		<div className="product-list">
+			{products.map(product => (
+				<article key={product._id}>
+					<strong>{product.title}</strong>
+					<p>{product.description}</p>
+					<a href="">Acessar</a>
+				</article>
+			))}
+		</div>
+		)
+	}
 }
 
 
